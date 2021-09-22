f00e0bf5ceb42aeb551ded10a089642a931bb91a
imraanishmail <imraanishmail@gmail.com>
2021-09-22 15:13:11 -0400

    Injection token

---
+++ src/app/providers.ts
@@ -0,0 +1,8 @@
+import { InjectionToken } from "@angular/core";
+
+export const lookupListToken = new InjectionToken('LookupListToken');
+
+export const lookupLists = {
+    mediums: ['Movies', 'Series']
+  };
+
